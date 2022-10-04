a63e573b417b0e0a2f6ff2eb25ee08a72199cf1c
Sergey <sergeynagovitcin@list.ru>
2022-10-04 20:05:46 +0300

    Add max_length.php

---
+++ max_length.php
@@ -0,0 +1,38 @@
+<?php
+
+function randommas($num, $maxnum) {
+    $array = array();
+    for ($i = 0; $i < $num; $i ++) {
+        $array[$i] = rand(1, $maxnum);
+    }
+    print_r($array);
+
+    $mas1 = array();
+    $mas2 = array();
+    
+
+    for ($i = 0; $i < count($array) - 1; $i ++) {
+        if ($array[$i] < $array[$i + 1]) {
+            if (count($mas1) > 0){
+                array_pop($mas1);
+            }
+            $mas1[] = $array[$i]; 
+            $mas1[] = $array[$i+1];
+           
+        }
+
+        else{
+            if (count($mas2) < count($mas1)){
+                $mas2 = $mas1;
+                
+            }
+            $mas1 = array();
+
+        }
+    }
+    print_r($mas2);
+
+    
+
+}
+randommas(100, 100);
